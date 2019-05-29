223496bbb1b636919c696a1155fd700bddf2daff
Damian Kieltyka <damian.kieltyka@icloud.com>
2019-05-29 22:29:45 +0200

    Add script
    js script add

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var data = [
     {
         id: 'box1',
@@ -30,3 +32,51 @@ var data = [
         categories: []
     },
 ];
+
+let createBox;
+let addData;
+let runStyles;
+let bElements;
+const body = document.getElementById('body');
+const highlighted = 'highlighted';
+const special_header = 'special-header';
+const imp = 'important';
+
+createBox = () => {
+    for (let i = 0; i < 5; i++) {
+        let box = '<div class="box"><div class="header"></div><div class="box-content"></div></div>';
+        body.innerHTML = box + body.innerHTML;
+    }
+};
+
+addData = () => {
+    for (let i = 0; i < 5; i++) {
+        let dataObj = data[i];
+        let header = document.querySelectorAll('.header')[i];
+        let content = document.querySelectorAll('.box-content')[i];
+        bElements = document.querySelectorAll('.box')[i];
+        bElements.id = dataObj.id;
+        header.innerHTML = dataObj.title;
+        content.innerHTML = dataObj.content;
+    }
+};
+
+runStyles = () => {
+    for (let i = 0; i < 5; i++) {
+        bElements = document.querySelectorAll('.box')[i];
+        let bHeader = document.querySelectorAll('.header')[i];
+        if (data[i].categories.includes(highlighted)) {
+            bElements.className += " highlighted";
+        }
+        if (data[i].categories.includes(imp)) {
+            bElements.className += " important";
+        }
+        if (data[i].categories.includes(special_header)) {
+            bHeader.className += " special-header";
+        }
+    }
+};
+
+createBox();
+addData();
+runStyles();
